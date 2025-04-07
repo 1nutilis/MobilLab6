aba4a538a75bb59f97b75eb3399ff19b96eedbe2
1nutilis <145498715+1nutilis@users.noreply.github.com>
2025-04-07 21:55:40 +0500

    Add files via upload

---
+++ main/java/com/example/lifecycle/MainActivity.kt
@@ -0,0 +1,47 @@
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lifecycle.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showToast("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showToast("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showToast("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showToast("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showToast("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showToast("onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showToast("onRestart")
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
